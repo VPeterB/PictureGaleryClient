61e1f70d75b2f6795fb8c50f1774855149eff2b5
Vida Péter <vida.peter1127@gmail.com>
2024-05-26 12:05:00 +0200

    heroku try6

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/dist/PGClient'));
+app.use(express.static(__dirname + '/dist/pgclient/browser'));
 app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/PGClient/index.html');
+  res.status(200).sendFile(__dirname + '/dist/pgclient/browser/index.html');
 });
 app.listen(process.env.PORT || 8080);
